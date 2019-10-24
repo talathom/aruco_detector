2190035c353c01f2fe782437613bf62f1ca98deb
Thomas Talasco <34171760+talathom@users.noreply.github.com>
2019-10-24 15:45:23 -0400

    Add files via upload
    Added new documentation and removed a magic number

--- scripts/Aruco_Detector.py
+++ scripts/Aruco_Detector.py
@@ -15,6 +15,7 @@ from AR_Marker import AR_Marker
 import math
     
 message = ArucoMessage()
+METERS_PER_FEET = 3.281
 
 class Aruco_Detector:
     def __init__(self):
@@ -35,37 +36,45 @@ class Aruco_Detector:
     def depth_callback(self, data):
         try:
             self.depth_image = np.asarray(self.bridge.imgmsg_to_cv2(data, '32FC1'))
+            # Build a cv_image from the data in the depth image callback
         except CvBridgeError as e:
             print(e)
             
     def image_callback(self, data):
         try:
             cv_image = np.asarray(self.bridge.imgmsg_to_cv2(data, "bgr8"))
+            #build a color image and check for AR markers in it
         except CvBridgeError as e:
             print(e)
             
         # Get image height and width
         img_shape = cv_image.shape
-        self.img_y = img_shape[0]
-        self.img_x = img_shape[1]
+        self.img_y = img_shape[0] #Image length
+        self.img_x = img_shape[1] #Image width
+        
+        #Image midpoints
         self.img_center_y = self.img_y/2
         self.img_center_x = self.img_x/2
         
-                
+        # Detect markers using OpenCV library        
         corners, ids, rejectedImgPoints = aruco.detectMarkers(cv_image, self.ar_dict, parameters=self.param)
         for i in range(0, len(corners)):
+            # Build list of marker objects
             new_marker = AR_Marker(corners, int(ids[i]))
             self.markers.append(new_marker)
             
         for marker in self.markers:
-            print(str(len(data.data)))
-            marker.z = self.depth_image[int(marker.y)][int(marker.x)]*3.281
-            if math.isnan(marker.z):
+            marker.z = self.depth_image[int(marker.y)][int(marker.x)]*METERS_PER_FEET #Converts meters to feet
+            if math.isnan(marker.z): # Check if marker is too close
                 marker.z = -1
+                
+            #Build message
             message.x = marker.x
             message.y = marker.y
             message.z = marker.z
             message.id = marker.id_num
+            
+            #Calculate angular coord by finding the angle between the vector which points to the center of the marker and the vector which represents the midpoint of the image
             vector_mult = self.img_center_x*marker.x + self.img_center_y*marker.y
             vector_magnitudes = self.getVectorMagnitude(self.img_center_x, self.img_center_y) * self.getVectorMagnitude(marker.x, marker.y)
             
@@ -74,11 +83,15 @@ class Aruco_Detector:
                 marker.angular = -marker.angular
             
             message.angular = marker.angular
+            
+            
             self.pub.publish(message)
         
+        #Clear marker list for next callback
         del self.markers[:]
         
     def getVectorMagnitude(self, x, y):
+        #Distance formula
         return math.sqrt(pow(x, 2) + pow(y, 2))
             
 if __name__ == '__main__':
